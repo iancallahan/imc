9525cab1e0ce42b4e90ecf485883a8ff08ad009e
Ian Callahan <ian@greatfortress.com>
2025-04-13 15:57:45 -0400

    Fix CV

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -107,7 +107,7 @@ class extends Component {
                 </a>
             </li>
             <li>
-                <a href="{{ asset('storage/Callahan_CV.pdf') }}" target="_blank" class="text-white hover:text-gray-300">
+                <a href="{{ asset('storage/callahan_CV.pdf') }}" target="_blank" class="text-white hover:text-gray-300">
                     <span class="sr-only">CV (PDF)</span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-github w-4 md:w-8" viewBox="0 0 16 16">
                         <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0m2 5.755V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-.245S4 12 8 12s5 1.755 5 1.755"/>
